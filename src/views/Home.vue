5dacccbad8ffd9dcb03a48078d07f850ca646e72
lin <kokodayo@bakaya.ro>
2021-12-29 12:41:03 +0900

    update style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="main-container">
     <div class="container-left">
+      <h1 class="pointing-prize">{{pointingPrize}}</h1>
       <div class="wheel-area">
         <div class="wheel-wrapper">
           <canvas ref="wheelCanvas" width="1000" height="1000" :style="`transform: rotate(${wheelRotate}turn)`" />
@@ -8,28 +9,24 @@
             <polygon v-if="prizeList.length" points="0,16 100,0 100,32" style="fill:#ffffff;stroke:#000000;stroke-width:1" />
           </svg>
         </div>
-        <div class="dashboard">
-          <h2>{{pointingPrize}}</h2>
-          <div v-if="prizeList.length" class="button-container">
-            <button class="start-roll" @click="startRoll" :disabled="rolling">&nbsp;转起！</button>
-          </div>
-        </div>
-        <div class="log-action">
-          <button @click="clearLog">清空记录</button>
-        </div>
+      </div>
+      <div v-if="prizeList.length" class="button-container">
+        <button class="start-roll" @click="startRoll" :disabled="rolling">&nbsp;转起！</button>
+      </div>
+    </div>
+    <div class="container-right">
+      <div class="prize-list">
+        <h4>转盘设置：</h4>
+        <textarea :readonly="rolling" v-model="rawPrizes" ref="prizeInput" />
       </div>
       <div class="roll-log">
         {{rollRecordLog}}
       </div>
-    </div>
-    <div class="prize-list">
-      <h4>转盘设置：</h4>
-      <div v-if="depositKey" class="prize-action">
-        <button @click="loadDeposit" :disabled="rolling">重置为预设</button>
-      </div>
-      <textarea :readonly="rolling" v-model="rawPrizes" ref="prizeInput" />
-      <div class="github-link">
-        <a target="_blank" href="https://github.com/regsvr32/prize-wheel">
+      <div class="footer">
+        <button v-if="depositKey" @click="loadDeposit" :disabled="rolling">重置为预设</button>
+        <button @click="clearLog">清空记录</button>
+        <span style="flex-grow: 1" />
+        <a class="github-link" target="_blank" href="https://github.com/regsvr32/prize-wheel">
           <img :src="gitHubIcon" /> GitHub
         </a>
       </div>
@@ -46,22 +43,24 @@ body
   width: 100vw
   height: 100vh
   display: flex
+  overflow: hidden
   .container-left
     display: flex
-    width: calc(100% - 360px)
     flex-direction: column
+    flex-grow: 1
     border-right: 1px solid #cccccc
+    .pointing-prize
+      text-align: center
+      color: #2acb2a
     .wheel-area
-      position: relative
-      padding: 20px 40px
-      border-bottom: 1px solid #cccccc
       width: 100%
-      height: calc(100% - 300px)
-      display: flex
+      padding: 40px
+      height: calc(100% - 270px)
       .wheel-wrapper
         position: relative
         max-width: 100%
         max-height: 100%
+        margin: auto
         aspect-ratio: 1 / 1
         canvas
           width: 100%
@@ -72,69 +71,66 @@ body
           top: 48.4%
           height: 3.2%
           width: 20%
-      .dashboard
-        flex-grow: 1
-        padding-left: 40px
-        text-align: center
-        overflow: auto
-        h2
-          color: #2acb2a
-        .button-container
-          margin: 100px 0px 20px
-          .start-roll
-            font-size: 24px
-            padding: 10px 30px
-            background-color: #719ece
-            border: 1px solid #666666
-            color: #3b2b55
-            border-radius: 4px 
-            &:hover
-              background-color: #b3cbe5
-              color: #60468b
-              transform: scale(1.1)
-            &:active
-              transform: unset
-            &:disabled
-              background-color: #eeeeee
-              color: #777777
-              transform: unset
-      .log-action
-        position: absolute
-        bottom: 12px
-        right: 12px
-    .roll-log
-      position: relative
+    .button-container
       flex-grow: 1
-      overflow: auto
+      padding-top: 60px
+      text-align: center
+      .start-roll
+        font-size: 24px
+        padding: 10px 30px
+        background-color: #719ece
+        border: 1px solid #666666
+        color: #3b2b55
+        border-radius: 4px 
+        &:hover
+          background-color: #b3cbe5
+          color: #60468b
+          transform: scale(1.1)
+        &:active
+          transform: unset
+        &:disabled
+          background-color: #eeeeee
+          color: #777777
+          transform: unset
+  .container-right
+    display: flex
+    flex-direction: column
+    width: 25%
+    min-width: 360px
+    .prize-list
+      display: flex
+      flex-direction: column
       padding: 20px
+      flex-grow: 1
+      border-bottom: 1px solid #cccccc
+      h4
+        margin: 0px 0px 12px
+      textarea
+        flex: 1
+        width: 100%
+        resize: none
+        font-size: 14px
+        border: none
+        &:focus
+          outline-color: #719ece
+    .roll-log
+      padding: 20px
+      flex: 1
+      overflow: auto
       white-space: pre-wrap
-  .prize-list
-    position: relative
-    flex-grow: 1
-    padding: 20px
-    h4
-      margin: 0px 0px 12px
-    .prize-action
-      position: absolute
-      top: 18px
-      right: 20px
-    textarea
-      width: 100%
-      resize: none
-      font-size: 14px
-      height: calc(100% - 60px)
-      border: none
-      &:focus
-        outline-color: #719ece
-    .github-link
-      margin-top: 8px 
-      text-align: end
-      a
+      border-bottom: 1px solid #cccccc
+    .footer
+      display: flex
+      padding: 12px
+      &>*
+        margin-right: 12px
+      .github-link
         text-decoration: none
         color: #0969da
         img
           vertical-align: text-top
           height: 1.2em
+
 </style>
 
 <script setup>
